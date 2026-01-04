0b3bb7f70008f7dd6dbf6076beef85f7e4aad58f
shiningCloud2025 <shining_cloud2025@163.com>
2026-01-04 10:28:11 +0800

    Hot100-2

--- AlgorithmTraining-Hot100/src/hot10/Solution2.java
+++ AlgorithmTraining-Hot100/src/hot10/Solution2.java
@@ -1,4 +1,12 @@
-  public List<List<String>> groupAnagrams(String[] strs) {
+package hot10;
+
+import java.util.*;
+
+/**
+ * 2.字母异位词分组
+ */
+public class Solution2 {
+    public List<List<String>> groupAnagrams(String[] strs) {
         List<List<String>> res = new LinkedList();
         Map<String,List<String>> map = new HashMap();
         for(String str:strs){
@@ -6,19 +14,20 @@
             Arrays.sort(cs);
             String newStr = new String(cs);
             if(map.containsKey(newStr)){
-                List<String> temp = null;
+                List<String> temp = new LinkedList<>();
                 temp = map.get(newStr);
                 temp.add(str);
                 map.put(newStr,temp);
             }else{
-                List<String> temp = null;
-                temp.add(str)
+                List<String> temp = new LinkedList<>();
+                temp.add(str);
                 map.put(newStr,temp);
             }
         }
         for(String str : map.keySet()){
             res.add(map.get(str));
         }
-        return res
+        return res;
 
-   }
+    }
+}

48b78d44d115b9b89ab5ba21bb08038da3797738
shiningCloud2025 <shining_cloud2025@163.com>
2026-01-04 10:12:54 +0800

    Hot100-1

---
+++ AlgorithmTraining-Hot100/src/hot10/Solution1.java
@@ -0,0 +1,22 @@
+package hot10;
+
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * 1.两数之和
+ * @author shiningCloud2025
+ */
+public class Solution1 {
+    public int[] twoSum(int[] nums, int target) {
+        Map<Integer,Integer> map = new HashMap();
+        for(int i=0;i<nums.length;i++){
+            if(map.containsKey(target-nums[i])){
+                return new int[]{i,map.get(target-nums[i])};
+            }
+            map.put(nums[i],i);
+        }
+        return null;
+    }
+}

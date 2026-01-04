b952f8fa2305c44a8417845098e67c6b8ba20acc
shiningCloud2025 <shining_cloud2025@163.com>
2026-01-04 21:55:11 +0800

    前缀和模版

---
+++ AlgorithmTraining-BaseTemplate/src/qianzuihe/Template.java
@@ -0,0 +1,18 @@
+package qianzuihe;
+
+/**
+ * 前缀和模版
+ * @author shiningCloud2025
+ */
+public class Template {
+    // TODO:前缀和是一种非常朴素的数据结构。主要用于:快速计算某一个区间的和
+    //  例题: nums=[1,2,3,4,5],我们统计前缀和数组pres=[0,1,3,6,10,15],那么:intervals[i,j]=pres[j+1]-pres[i]
+
+    // TODO:前缀和模版代码
+//    int[] nums;
+//    int n;
+//    int[] pres = new int[n+1];
+//    for(int i=0;i<n;i++){
+//        pres[i+1] = pres[i]+nums[i];
+//    }
+}

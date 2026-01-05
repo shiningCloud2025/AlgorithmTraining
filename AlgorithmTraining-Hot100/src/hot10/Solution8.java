b0d6ac002ab5c5b7ee545f530bf436f9ff9535e9
shiningCloud2025 <shining_cloud2025@163.com>
2026-01-05 14:18:31 +0800

    Hot100-8

--- AlgorithmTraining-Hot100/src/hot10/Solution8.java
+++ AlgorithmTraining-Hot100/src/hot10/Solution8.java
@@ -1,9 +1,25 @@
 package hot10;
 
+import java.util.HashSet;
+import java.util.Set;
+
 /**
  * 8.无重复字符的最长子串
  * @author shiningCloud2025
  */
 public class Solution8 {
-
+    public int lengthOfLongestSubstring(String s) {
+        // TODO:核心思路其实是先判断是否有重复，有的话左指针右移,没有重复的话，右移统计即可
+        int res = 0;
+        Set<Character> set = new HashSet();
+        char[] cs = s.toCharArray();
+        for(int l=0,r=0;r<cs.length;r++){
+            while(l<r&&set.contains(cs[r])){
+                set.remove(cs[l++]);
+            }
+            set.add(cs[r]);
+            res = Math.max(res,r-l+1);
+        }
+        return res;
+    }
 }

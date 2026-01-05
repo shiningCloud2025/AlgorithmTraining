e95ac1d77ac221bd462a70154332ba7182a54240
shiningCloud2025 <shining_cloud2025@163.com>
2026-01-05 14:36:24 +0800

    单调栈模版

---
+++ AlgorithmTraining-BaseTemplate/src/dandiaostackandqueue/Template1.java
@@ -0,0 +1,19 @@
+package dandiaostackandqueue;
+
+/**
+ * 单调栈模版
+ * @author shiningCloud2025
+ */
+public class Template1 {
+    // TODO:单调栈是栈内元素满足单调性(单调递增/单调递减)的数据结构.时间复杂度是O(n).主要用于:Next-Greater(找下一个更大元素/下一个更小元素)
+
+    // 单调栈代码模版
+//    stack; // 单调栈
+//    for(int i=0;i<n;i++){
+//        while(stack&&num[stack.top]<nums[i]){
+//            int top = stack.pop();
+//            // 此时说明nums[top]的下一个更大元素为nums[i];
+//        }
+//        stack.push(i);
+//    }
+}
---
+++ AlgorithmTraining-BaseTemplate/src/dandiaostackandqueue/Template2.java
@@ -0,0 +1,4 @@
+package dandiaostackandqueue;
+
+public class Template2 {
+}

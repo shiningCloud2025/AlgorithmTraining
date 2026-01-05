75396ae357dc7cde2c84b78c95cb78ff1c0b7bc5
shiningCloud2025 <shining_cloud2025@163.com>
2026-01-05 14:48:49 +0800

    单调队列模版

--- AlgorithmTraining-BaseTemplate/src/dandiaostackandqueue/Template2.java
+++ AlgorithmTraining-BaseTemplate/src/dandiaostackandqueue/Template2.java
@@ -1,4 +1,18 @@
 package dandiaostackandqueue;
 
+/**
+ * 单调队列模版
+ * @author shiningCloud2025
+ */
 public class Template2 {
+    // TODO:单调队列是队列中的元素满足单调性(单调递增/递减)的数据结构,时间复杂度为O(n).主要用于:滑动窗口的最大值(最小值)
+
+    // TODO:单调队列代码模版
+//    queue;// 单调队列
+//    for(int i=0;i<n;i++){
+//        if(窗口达到上限) q.popleft();
+//        while(queue&&nums[q.back]>=nums[i])  queue.pop();
+//        queue.push(i);
+//    }
+
 }

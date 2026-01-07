fdaa6995274136a09516ec7c8296ba64aeb17ada
shiningCloud2025 <shining_cloud2025@163.com>
2026-01-07 15:26:02 +0800

    Hot100-15

--- AlgorithmTraining-Hot100/src/hot20/Solution15.java
+++ AlgorithmTraining-Hot100/src/hot20/Solution15.java
@@ -1,8 +1,28 @@
 package hot20;
 
+import java.util.Arrays;
+
 /**
  * 15.轮转数组
  * @author shiningCloud2025
  */
 public class Solution15 {
+    public void rotate(int[] nums, int k) {
+        // TODO:需要注意的点:
+        //  1.k要做处理,本质是因为如果k>=length的话,其实是一个周期了,是不用算的
+        //  2.然后做整体调换和区间调换就可以了
+        k = k%nums.length;
+        reverse(0,nums.length-1,nums);
+        reverse(0,k-1,nums);
+        reverse(k,nums.length-1,nums);
+    }
+    public void reverse(int start,int end,int[] nums){
+        while(start<end){
+            int temp = nums[start];
+            nums[start] = nums[end];
+            nums[end] = temp;
+            start++;
+            end--;
+        }
+    }
 }
--- AlgorithmTraining-Hot100/src/hot20/Solution16.java
+++ AlgorithmTraining-Hot100/src/hot20/Solution16.java
@@ -4,4 +4,21 @@ package hot20;
  * 16.除了自身以外数组的乘积
  */
 public class Solution16 {
+    public int[] productExceptSelf(int[] nums) {
+        int[] res = new int[nums.length];
+        int[] frontJ = new int[nums.length];
+        int[] lastJ = new int[nums.length];
+        frontJ[0] = 1;
+        lastJ[nums.length-1] = 1;
+        for(int i=1;i<nums.length;i++){
+            frontJ[i] = frontJ[i-1]*nums[i-1];
+        }
+        for(int i=nums.length-2;i>=0;i--){
+            lastJ[i] = lastJ[i+1]*nums[i+1];
+        }
+        for(int i=0;i<res.length;i++){
+            res[i] = frontJ[i]*lastJ[i];
+        }
+        return res;
+    }
 }
